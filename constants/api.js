b28f4d630b1060e25a5c06d306fcd08dfcba2227
Rishav-bot895 <rishav08092005@gmail.com>
2026-02-08 13:00:03 +0530

    masked ip

--- constants/api.js
+++ constants/api.js
@@ -1,4 +1,4 @@
 export const BASE_URL = `https://attendance-app-backend-p9ce.onrender.com`;
 
 // Debug log ingest (Cursor). On physical devices, set to your PC's IP so phones can send logs (e.g. 'http://192.168.1.100:7242').
-export const DEBUG_LOG_INGEST = `http://192.168.29.28:7242/ingest/df8d7e14-cf47-40d8-8b34-56fe154df2b4`;
+export const DEBUG_LOG_INGEST = ``;
